5fcc1baa1633d2c2da778644a6eb97de6573c587
nownabe <nownabe@gmail.com>
2017-12-02 00:07:07 +0900

    3.3 イニシャライザ

--- Rational/src/Main.kt
+++ Rational/src/Main.kt
@@ -4,4 +4,10 @@ fun main(args: Array<String>) {
     val half = Rational(1, 2)
     println(half.denominator)
     println(half)
+
+    try {
+        Rational(1, 0)
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
--- Rational/src/Rational.kt
+++ Rational/src/Rational.kt
@@ -1,5 +1,9 @@
 package rational
 
 class Rational(val numerator: Int, val denominator: Int) {
+    init {
+        require(denominator != 0, {"denominator must not be zero"})
+    }
+
     override fun toString() = "$numerator/$denominator"
 }
